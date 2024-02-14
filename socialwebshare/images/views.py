e032a03b5bc7759676a2c3dd76b2d2bf9e87424d
Rafael <rafael_dvid@hotmail.com>
2024-02-14 19:28:34 -0300

    generating docker files and creating image detail page

--- socialwebshare/images/views.py
+++ socialwebshare/images/views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
 from images.forms import ImageCreateForm
+from .models import Image
 
 
 @login_required
@@ -16,9 +16,17 @@ def image_create(request):
             new_image.save()
             messages.success(request,
                              'Image added successfully')
-            return redirect(new_image.get_absolute.url())
+            return redirect(new_image.get_absolute_url())
     else:
         form = ImageCreateForm(data=request.GET)
     return render(request,
                   'images/image/create.html',
                   {'section': 'images', 'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
