156f983575662f6abca7489369c42d411d7ba73e
Rafael <rafael_dvid@hotmail.com>
2024-02-14 22:30:15 -0300

    add like/unlike functionality with DOM JS csrf cookie validation

--- socialwebshare/images/views.py
+++ socialwebshare/images/views.py
@@ -1,6 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from requests import Response
+
 from images.forms import ImageCreateForm
 from .models import Image
 
@@ -30,3 +34,22 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.user_likes.add(request.user)
+            else:
+                image.user_likes.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
