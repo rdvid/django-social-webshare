9766ee21e330c5df252dc1f7ea36b31b43a9d946
Rafael <rafael_dvid@hotmail.com>
2024-02-14 23:16:06 -0300

    add scroll event and image list with pagination

--- socialwebshare/images/views.py
+++ socialwebshare/images/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from requests import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from images.forms import ImageCreateForm
 from .models import Image
@@ -53,3 +54,27 @@ def image_like(request):
             pass
     return JsonResponse({'status': 'error'})
 
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images',
+                   'images': images})
